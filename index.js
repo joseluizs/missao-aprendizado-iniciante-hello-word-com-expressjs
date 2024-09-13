f2e35f57da4b265ac0991d381b36189e05e2aabb
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-13 12:50:41 +0000

    Endpoint '/oi' adicionado & criação do script start

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.get('/oi', function (req, res) {
+  res.send('Olá Mundo')
+})
+
+app.listen(3000, function () {
+  console.log('Aplicação rodando em http://localhost:3000')
+})
